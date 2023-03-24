5f424921904d61da30c8dd2faeafa8e37983b562
Danial Masruroini <danial@smktelkom-mlg.sch.id>
2023-03-25 06:17:11 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -2,14 +2,14 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    routes: {
-      cors: {
-        origin: ['*'],
-      },
+  port: 5000,
+  host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+  routes: {
+    cors: {
+      origin: ['*'],
     },
-  });
+  },
+});
 
 const init = async () => {
   server.route(routes);
